644c856736a460b7572cf2f1f9609caeb1b984f6
Kieran Duffy <kierangerard.duffy@gmail.com>
2021-10-23 15:32:34 +0100

    Moved data back to the Controller

--- app_server/controllers/details.js
+++ app_server/controllers/details.js
@@ -1,6 +1,33 @@
 /* GET details page */
 const cryptopage = function(req, res){ 
-res.render('crypto_data', { title: 'Crypto Details' }); 
+	res.render('crypto_data', { 
+		title: 'Crypto Details',
+		card: { 
+			price: 'Price: ', 
+			volume: 'Volume: ',
+			volMarket: 'Vol/Marketcap: '
+				},
+		Cryptos: [{
+			name: 'Bitcoin',
+			price: '60,586',
+			volume: '43,241,820',
+			marketcap: '0.037'},
+		    
+			{name: 'Ethereum',
+			price: '3,586',
+			volume: '22,671,110',
+			marketcap: '0.011'},
+			
+			{name: 'Cardano',
+			price: '3.41',
+			volume: '3,211,413',
+			marketcap: '0.008'},
+			
+			{name: 'XRP',
+			price: '1.96',
+			volume: '1,241,820',
+			marketcap: '0.001'}]	
+	}); 
 };
 module.exports = { 
 cryptopage 
--- app_server/controllers/sign_in.js
+++ app_server/controllers/sign_in.js
@@ -1,11 +1,36 @@
 /* GET 'home' page */
 const signinpage = function(req, res){
-res.render('index', { title: 'Sign In' });
+	res.render('index', { 
+		title: 'Sign In', 
+		header: { 
+			title: 'Welcome to Crypto Price', 
+			tagLine: 'Login to your account below' 
+				},
+		loginForm: {
+			email: 'Email address:',
+			password: 'Password:',
+			button: 'Login',
+			link: 'Register Here'
+		}		
+	});
 };
 
 /* GET 'registration page' page */
 const registrationpage = function(req, res){
-res.render('register', { title: 'Register' });
+	res.render('register', { 
+		title: 'Register',
+		header: { 
+			title: 'Welcome to Crypto Price', 
+			tagLine: 'Register account below' 
+				},
+		registerForm: {
+			email: 'Email address:',
+			firstname: 'First Name:',
+			surname: 'Surname:',
+			password: 'Password:',
+			button: 'Register'
+		}
+	});
 };
 
 module.exports = {
